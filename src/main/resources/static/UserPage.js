08a50ed32b31f9cc6a376e8559e42dfc73c7c9f1
Dunkelzahn <cccp010100101@gmail.com>
2022-12-15 21:51:33 +0400

    ver whiskytangofoxtrot2

--- src/main/resources/static/UserPage.js
+++ src/main/resources/static/UserPage.js
@@ -5,18 +5,17 @@ async function thisUser() {
     fetch("http://localhost:8080/api/viewUser")
         .then(res => res.json())
         .then(data => {
-            // Добавляем информацию в шапку
             $('#headerUsername').append(data.username);
             let roles = data.roles.map(role => " " + role.role.substring(5));
             $('#headerRoles').append(roles);
-
-            //Добавляем информацию в таблицу
+            
             let user = `$(
             <tr>
                 <td>${data.id}</td>
-                <td>${data.firstName}</td>
+                <td>${data.name}</td>
+                <td>${data.surname}</td>
                 <td>${data.password}</td>
-                <td>${data.city}</td>
+                <td>${data.age}</td>
                 <td>${data.email}</td>
                 <td>${roles}</td>)`;
             $('#userPanelBody').append(user);
